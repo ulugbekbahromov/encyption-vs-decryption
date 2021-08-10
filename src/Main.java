b8ec31297c45214ea0fde185cdaf310271c118f8
Ulugbek <ulugxodja@gmail.com>
2021-08-10 23:23:37 +0500

    added decryption functionality

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,55 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String command = scanner.nextLine();
         String sentence = scanner.nextLine();
         int key = scanner.nextInt();
 
-        System.out.println(encryptMessage(sentence, key));
+        switch (command) {
+            case "enc":
+                System.out.println(encryptMessage(sentence, key));
+                break;
+            case "dec":
+                System.out.println(decryptMessage(sentence, key));
+                break;
+        }
     }
 
     public static String encryptMessage(String message, int key) {
-        final int MAX_VALUE = 123;
-        final int MIN_VALUE = 96;
-        final int alphabetLength = 26;
 
         StringBuilder result = new StringBuilder();
 
         for (int index = 0; index < message.length(); index++) {
-            if (message.charAt(index) == ' ') {
-                result.append(' ');
-            } else if (Character.isLowerCase(message.charAt(index))) {
+            if (Character.isLowerCase(message.charAt(index))) {
                 char ch = (char) (((int) message.charAt(index) +
-                        key - MIN_VALUE) % alphabetLength + MIN_VALUE);
+                        key));
                 result.append(ch);
             } else {
                 char ch = (char) (((int) message.charAt(index) +
-                        key - MAX_VALUE) % alphabetLength + MAX_VALUE);
+                        key));
                 result.append(ch);
             }
         }
 
+        return result.toString();
+    }
+
+    public static String decryptMessage(String message, int key) {
+
+        StringBuilder result = new StringBuilder();
+
+        for (int index = 0; index < message.length(); index++) {
+            if (Character.isLowerCase(message.charAt(index))) {
+                char ch = (char) (((int) message.charAt(index) -
+                        key));
+                result.append(ch);
+            } else {
+                char ch = (char) (((int) message.charAt(index) -
+                        key));
+                result.append(ch);
+            }
+        }
         return result.toString();
     }
 }
