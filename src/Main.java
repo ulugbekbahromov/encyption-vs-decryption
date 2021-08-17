9f5c4f4f320959ba16f397f4d322877237143846
Ulugbek <ulugxodja@gmail.com>
2021-08-17 17:51:51 +0500

    modify program to read and write date to files

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,71 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Command message = new Command();
+        String choice = "enc";
+        String text = "";
+        boolean isData = false;
+        int key = 0;
+        String file_to_read = "";
+        boolean isIn = false;
+        String file_to_write = "";
+        boolean isOut = false;
 
-        for (int i = 0; i < args.length; i++) {
+        for (int i = 0; i < args.length; i += 2) {
             switch (args[i]) {
                 case "-mode":
-                    message.setUserChoice(args[i + 1]);
+                    choice = args[i + 1];
                     break;
                 case "-key":
-                    message.setShiftNumber(args[i + 1]);
+                    key = Integer.parseInt(args[i + 1]);
                     break;
                 case "-data":
-                    message.setMessage(args[i + 1]);
+                    text = args[i + 1];
+                    isData = true;
+                    break;
+                case "-in":
+                    file_to_read = args[i + 1];
+                    isIn = true;
+                    break;
+                case "-out":
+                    file_to_write = args[i + 1];
+                    isOut = true;
                     break;
             }
         }
 
-        switch (message.userChoice) {
-            case "enc":
-                message.encryptMessage();
-                break;
-            case "dec":
-                message.decryptMessage();
-                break;
+        if (!isData && !isIn) {
+            System.out.print("Error");
         }
 
-        message.printResult();
+        if (!isData && isIn) {
+            File in = new File(file_to_read);
+            try (Scanner file_s = new Scanner(in)) {
+                text = file_s.nextLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String res;
+        if (choice.equals("enc")) {
+            res = Encoder.encode(text, key);
+        } else {
+            res = Decoder.decode(text, key);
+        }
+        if (isOut) {
+            File out = new File(file_to_write);
+            try (FileWriter writer = new FileWriter(out)) {
+                writer.write(res);
+            } catch (IOException e) {
+                System.out.printf("An exception occurs %s", e.getMessage());
+            }
+        }
+        if (!isOut) {
+            System.out.println(res);
+        }
+
     }
 }
