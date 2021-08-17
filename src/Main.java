93127142e288ef5943684280ad710b5b0700da43
Ulugbek <ulugxodja@gmail.com>
2021-08-17 18:01:59 +0500

    modify program to read and write date to files

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String choice = "enc";
+        String option = "enc";
         String text = "";
         boolean isData = false;
         int key = 0;
-        String file_to_read = "";
+        String fileToRead = "";
         boolean isIn = false;
-        String file_to_write = "";
+        String fileToWrite = "";
         boolean isOut = false;
 
         for (int i = 0; i < args.length; i += 2) {
             switch (args[i]) {
                 case "-mode":
-                    choice = args[i + 1];
+                    option = args[i + 1];
                     break;
                 case "-key":
                     key = Integer.parseInt(args[i + 1]);
@@ -26,22 +26,22 @@ public class Main {
                     isData = true;
                     break;
                 case "-in":
-                    file_to_read = args[i + 1];
+                    fileToRead = args[i + 1];
                     isIn = true;
                     break;
                 case "-out":
-                    file_to_write = args[i + 1];
+                    fileToWrite = args[i + 1];
                     isOut = true;
                     break;
             }
         }
 
         if (!isData && !isIn) {
-            System.out.print("Error");
+            System.out.println("Error !!!");
         }
 
-        if (!isData && isIn) {
-            File in = new File(file_to_read);
+        if (!isData && !isIn) {
+            File in = new File(fileToRead);
             try (Scanner file_s = new Scanner(in)) {
                 text = file_s.nextLine();
             } catch (Exception e) {
@@ -49,23 +49,23 @@ public class Main {
             }
         }
 
-        String res;
-        if (choice.equals("enc")) {
-            res = Encoder.encode(text, key);
+        String response;
+        if (option.equals("enc")) {
+            response = Encoder.encode(text, key);
         } else {
-            res = Decoder.decode(text, key);
+            response = Decoder.decode(text, key);
         }
         if (isOut) {
-            File out = new File(file_to_write);
+            File out = new File(fileToWrite);
             try (FileWriter writer = new FileWriter(out)) {
-                writer.write(res);
+                writer.write(response);
             } catch (IOException e) {
                 System.out.printf("An exception occurs %s", e.getMessage());
             }
         }
-        if (!isOut) {
-            System.out.println(res);
-        }
 
+        if (!isOut) {
+            System.out.println(response);
+        }
     }
 }
